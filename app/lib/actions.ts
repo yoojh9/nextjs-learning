f82208f41f2818a4fb297bb007043918166bae21
yoojh9 <yoojh9@gmail.com>
2023-11-16 16:36:06 +0900

    Adding the sign in

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
 
 const InvoiceScheme = z.object({
   id: z.string(),
@@ -119,4 +120,18 @@ export const deleteInvoice = async (id: string) => {
   }
 
   revalidatePath("/dashboard/invoices");
+};
+
+export const authenticate = async (
+  prevState: string | undefined,
+  formData: FormData
+) => {
+  try {
+    await signIn("credentials", Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialSignin";
+    }
+    throw error;
+  }
 };
